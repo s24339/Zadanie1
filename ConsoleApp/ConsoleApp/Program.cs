6382ef29ec5ce63a8173b8b36dab45af822097ca
s24339 <s24339@pjwstk.edu.pl>
2024-04-12 10:25:52 +0200

    Adding calculateAVG method

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Mod -> 3!");
+public static double calculateAVG(int[] nums){
+    int sum = 0;
+    if(nums.Length == 0){
+        return 0;
+    }else{
+        foreach(int num in nums){
+            sum += num;
+        }
+    }
+    return (double)sum/nums.Length
+}
+
